4795e2b44ae8dda95edf20e1e73f4274c9dac0cd
Michael Bassous <mbassous@gmail.com>
2018-10-25 16:21:06 -0700

    Added UI to main activity screen, and linked first three buttons to the next activities.

--- app/src/main/java/mbccjlkn/whatsonthemenu/MainActivity.java
+++ app/src/main/java/mbccjlkn/whatsonthemenu/MainActivity.java
@@ -1,7 +1,9 @@
 package mbccjlkn.whatsonthemenu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void diningHall(View view){
+        Intent I = new Intent(this, DiningHallSelection.class);
+        startActivity(I);
+    }
+
+    public void cafe(View view){
+        Intent I = new Intent(this, DiningHallSelection.class);
+        startActivity(I);
+    }
+
+    public void foodTrucks(View view){
+        Intent I = new Intent(this, DiningHallSelection.class);
+        startActivity(I);
     }
 }
