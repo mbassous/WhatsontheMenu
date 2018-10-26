a1ad26f1d2a4ada91593544e032fe82f1bd1ca63
Michael Bassous <mbassous@gmail.com>
2018-10-25 17:13:20 -0700

    Added new activities for DH selection page, and added specific DHall buttons.

--- app/src/main/java/mbccjlkn/whatsonthemenu/DiningHallSelection.java
+++ app/src/main/java/mbccjlkn/whatsonthemenu/DiningHallSelection.java
@@ -1,7 +1,9 @@
 package mbccjlkn.whatsonthemenu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DiningHallSelection extends AppCompatActivity {
 
@@ -10,4 +12,11 @@ public class DiningHallSelection extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dining_hall_selection);
     }
+
+    public void startDHActivity(View view){
+        Intent I = new Intent(this, DHDisplay.class);
+        startActivity(I);
+    }
+
+
 }
