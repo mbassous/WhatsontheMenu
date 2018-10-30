1f7706673659e5f252caeb3d846ea7f621decb19
Michael Bassous <mbassous@ucsc.edu>
2018-10-29 17:00:34 -0700

    Fixed a couple ID's

--- app/src/main/java/mbccjlkn/whatsonthemenu/CafeSelection.java
+++ app/src/main/java/mbccjlkn/whatsonthemenu/CafeSelection.java
@@ -2,12 +2,22 @@ package mbccjlkn.whatsonthemenu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class CafeSelection extends AppCompatActivity {
+
+    //Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cafe_selection);
+
+        //spinner = findViewById(R.id.SpinnerCafe);
     }
+
+
+
+
+
 }
