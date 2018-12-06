d4fe311b5982f8e31d4a94f20fa806d52e1725ea
Justin Law <jlawhome08@gmail.com>
2018-12-05 19:22:51 -0800

    Revert "Revert "Merge branch 'usbproblems' into AvailabilityColors""
    This reverts commit 99d661f38123ca73881e904fb0656c85850dc717.

--- app/src/main/java/mbccjlkn/whatsonthemenu/FavoritesSelection.java
+++ app/src/main/java/mbccjlkn/whatsonthemenu/FavoritesSelection.java
@@ -1,70 +1,77 @@
 package mbccjlkn.whatsonthemenu;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Space;
 
 import java.util.ArrayList;
 
 public class FavoritesSelection extends AppCompatActivity {
 
-    ArrayList<Integer> fav = MainActivity.Favorites;
     public int i;
-    public String[] eateryNames = { "Cruz N' Gourmet",
-                                    "Drunk Monkey",
-                                    "Raymond's Catering",
-                                    "Banana Joe's (Crown)",
-                                    "Bowls (Porter)",
-                                    "College 8 Cafe",
-                                    "Cowell Coffee Shop",
-                                    "Express Store (Quarry)",
-                                    "Global Village Cafe (Mchenry)",
-                                    "Iveta (Quarry)",
-                                    "Kresge Co-op",
-                                    "Oakes Cafe",
-                                    "Owl's Nest (Kresge)",
-                                    "Perk Coffee (J Baskin)",
-                                    "Perk Coffee (Earth and Marine)",
-                                    "Perk Coffee (Physical Sciences)",
-                                    "Perk Coffee (Terra Fresca)",
-                                    "Stevenson Coffee House",
-                                    "Terra Fresca (College 9/10)",
-                                    "Vivas (Merrill)",
-                                    "College 9/10",
-                                    "Cowell/Stevenson",
-                                    "Crown/Merrill",
-                                    "Porter/Kresge",
-                                    "Rachel Carson/Oakes" };
+    public static String[] eateryNames = { "Cruz N' Gourmet",
+            "Drunk Monkey",
+            "Raymond's Catering",
+            "Banana Joe's (Crown)",
+            "Bowls (Porter)",
+            "College 8 Cafe",
+            "Cowell Coffee Shop",
+            "Express Store (Quarry)",
+            "Global Village Cafe (Mchenry)",
+            "Iveta (Quarry)",
+            "Kresge Co-op",
+            "Oakes Cafe",
+            "Owl's Nest (Kresge)",
+            "Perk Coffee (J Baskin)",
+            "Perk Coffee (Earth and Marine)",
+            "Perk Coffee (Physical Sciences)",
+            "Perk Coffee (Terra Fresca)",
+            "Stevenson Coffee House",
+            "Terra Fresca (College 9/10)",
+            "Vivas (Merrill)",
+            "College 9/10",
+            "Cowell/Stevenson",
+            "Crown/Merrill",
+            "Porter/Kresge",
+            "Rachel Carson/Oakes" };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites_selection);
 
-        ConstraintLayout cl = (ConstraintLayout)findViewById(R.id.FavCL);
         LinearLayout ll = (LinearLayout)findViewById(R.id.FavLL);
 
-        Button[] buttons = new Button[fav.size()];
+        SharedPreferences sp = this.getSharedPreferences("WOTM", Context.MODE_PRIVATE);
+        String spText = sp.getString("Info", "");
+        ArrayList<Integer> Fav = new ArrayList<Integer>();
+
+        String[] savedIds = spText.split("-");
+
+        for (int i = 0; i < savedIds.length; i++)
+            Fav.add(Integer.parseInt(savedIds[i]));
+
+        Button[] buttons = new Button[Fav.size()];
+        Space[] spaces = new Space[Fav.size()];
+
         for (i = 0; i < buttons.length; i++){
             LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT);
+                    200);
 
             buttons[i] = new Button(this);
-            buttons[i].setText(eateryNames[(fav.get(i) - 1)]);
+            buttons[i].setText(eateryNames[(Fav.get(i) - 1)]);
             buttons[i].setTextSize(17);
-            //buttons[i].setTextColor(024);
-            //buttons[i].setLayoutParams(new LinearLayout.LayoutParams(300, 60));
-            buttons[i].setWidth(300);
-            buttons[i].setHeight(60);
+            buttons[i].setTextColor(getResources().getColor(R.color.colorPrimaryDark));
             buttons[i].setBackgroundResource(R.drawable.rounded_button);
-            buttons[i].setId(fav.get(i));
-            //buttons[i].set
-            cl.addView(buttons[i], params);
+            buttons[i].setId(Fav.get(i));
+            ll.addView(buttons[i], params);
 
             buttons[i].setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
@@ -72,40 +79,39 @@ public class FavoritesSelection extends AppCompatActivity {
                         Intent I = new Intent(FavoritesSelection.this, CafeDisplay.class);
                         I.putExtra("id", (int)view.getId());
                         startActivity(I);
+                        //startActivity(new Intent(FavoritesSelection.this, FavoritesSelection.class));
                     } else {
-                        /*Intent I = new Intent(FavoritesSelection.this, DiningHallDisplay.class);
+                        Intent I = new Intent(FavoritesSelection.this, DiningHallDisplayPage.class);
                         I.putExtra("id", (int)view.getId());
-                        startActivity(I);*/
+                        startActivity(I);
+                        //startActivity(new Intent(FavoritesSelection.this, FavoritesSelection.class));
                     }
                 }
             });
+
+            spaces[i] = new Space(this);
+            spaces[i].setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 40));
+
+            ll.addView(spaces[i]);
         }
+    }
 
+    @Override
+    public void onRestart() {
+        super.onRestart();
 
+        SharedPreferences sp = this.getSharedPreferences("WOTM", Context.MODE_PRIVATE);
+        String spText = sp.getString("Info", "");
+        ArrayList<Integer> Fav = new ArrayList<Integer>();
 
-        /*
+        String[] savedIds;
+        if (spText.equals(""))
+            savedIds = new String[0];
+        else
+            savedIds = spText.split("-");
 
-
-            for (int i = 1; i <= 20; i++) {
-    LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-            LinearLayout.LayoutParams.MATCH_PARENT,
-            LinearLayout.LayoutParams.WRAP_CONTENT);
-    Button btn = new Button(this);
-    btn.setId(i);
-    final int id_ = btn.getId();
-    btn.setText("button " + id_);
-    btn.setBackgroundColor(Color.rgb(70, 80, 90));
-    linear.addView(btn, params);
-    btn1 = ((Button) findViewById(id_));
-    btn1.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View view) {
-            Toast.makeText(view.getContext(),
-                    "Button clicked index = " + id_, Toast.LENGTH_SHORT)
-                    .show();
-        }
-    });
-}
-
-         */
+        if (savedIds.length != 0)
+            startActivity(new Intent(FavoritesSelection.this, FavoritesSelection.class));
+        finish();
     }
 }
